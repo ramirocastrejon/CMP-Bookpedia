0b04fe405b709926722443be31ab246847d76b38
ramirocastrejon <ramiro.castrejon.jr@gmail.com>
2025-01-15 15:48:04 -0500

    initial starter

--- composeApp/src/commonMain/kotlin/com/ramiro/castrejon/App.kt
+++ composeApp/src/commonMain/kotlin/com/ramiro/castrejon/App.kt
@@ -24,7 +24,7 @@ fun App() {
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
             Button(onClick = { showContent = !showContent }) {
                 Text("Click me!!")
-            }git
+            }
             AnimatedVisibility(showContent) {
                 val greeting = remember { Greeting().greet() }
                 Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
