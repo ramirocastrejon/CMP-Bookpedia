ec65ecd073b6093a725f0b10535dddd5aaf69066
ramirocastrejon <ramiro.castrejon.jr@gmail.com>
2025-03-26 10:39:28 -0400

    bookdto

---
+++ composeApp/src/commonMain/kotlin/com/ramiro/castrejon/book/data/dto/SearchResponseDto.kt
@@ -0,0 +1,10 @@
+package com.ramiro.castrejon.book.data.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class SearchResponseDto(
+    @SerialName("docs")
+    val results: List<SearchedBookDto>
+)
